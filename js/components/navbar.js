0b8c3d029bf2661f983b0983a5b6449b73614365
Otávio Sudano <otavio.sudano13@gmail.com>
2026-04-16 11:09:48 -0300

    update design and fix responsivity

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -6,5 +6,12 @@ menuButton.addEventListener('click', () => {
     menu.classList.toggle('navbar__menu--active');
     menuIcon.classList.toggle('iconoir-menu');
     menuIcon.classList.toggle('iconoir-xmark');
-    menu.classList.toggle('animate__fadeInRight');
+
+    if (menu.classList.contains('navbar__menu--active')) {
+        menu.style.left = '0';
+        menu.style.opacity = '1';
+    } else {
+        menu.style.left = '100dvw';
+        menu.style.opacity = '0';
+    }
 });
